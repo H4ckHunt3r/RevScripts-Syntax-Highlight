4a6da8c746a0f9ac18df0a911a139b2e607813c5
H4ckHunt3r <webmaster@h4ckhunt3r.net>
2011-09-04 11:13:23 +0200

    (SecuretyFix) bug in css.php gefixt

--- RevScripts/Highlight/css.php
+++ RevScripts/Highlight/css.php
@@ -19,19 +19,19 @@ Properetys(Eigenschaften): 	.code-css .egs
 
 	function parseCode($code)
 	{
-		if(preg_match("/<style(*.)>(*.)<\/<style>/",$code))
-		{
-			$code = preg_replace("/<style(*.)>(*.)<\/<style>/","$2",$code);
-		}
+		
 		$cssRx1 = "/([A-z-_]*)\:([A-z0-9#()-_,\/\"'!.\%\\ ]*);/";
 		$cssRx2 = "/\'(.*)\'/siU";
 		$cssRx3 = "/\"(.*)\"/siU";
+			
+			$cssCode = str_replace("<", "&lt;", $code);
+			$cssCode = str_replace(">", "&gt;", $cssCode);
+			
+			$cssCode = preg_replace($cssRx3, "<span class=\"str\">&quot;$1&quot;</span>", $cssCode);
+			$cssCode = preg_replace($cssRx1, "<span class=\"egs\">$1</span>:<span class=\"val\">$2</span>;", $cssCode);
+			$cssCode = preg_replace($cssRx2, "<span class=\"str\">'$1'</span>", $cssCode);
 		
-		$cssCode = preg_replace($cssRx3, "<span class=\"str\">&quot;$1&quot;</span>", $cssCode);
-		$cssCode = preg_replace($cssRx1, "<span class=\"egs\">$1</span>:<span class=\"val\">$2</span>;", $code);
-		$cssCode = preg_replace($cssRx2, "<span class=\"str\">'$1'</span>", $cssCode);
-		
-		$cssCode = str_replace("!important", "<span class=\"important\">!important</span>", $cssCode);
+			$cssCode = str_replace("!important", "<span class=\"important\">!important</span>", $cssCode);
 		
 		return $cssCode;
 	}
